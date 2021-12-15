21c2949dd67e1e0e73fd7ac4838aaf99639d0412
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2021-12-16 05:26:55 +1100

    Done

--- machine.py
+++ machine.py
@@ -5,15 +5,11 @@ class Machine(nn.Module):
     def __init__(self):
         super(Machine, self).__init__()
         self.fc = nn.Sequential(
-            nn.Conv1d(1, 8, 5, stride=2),
-            nn.LeakyReLU(),
-            nn.MaxPool1d(3),
-            # nn.Conv1d(8, 16, 5),
-            # nn.LeakyReLU(),
-            # nn.MaxPool1d(3),
+            nn.Conv1d(1, 8, 5),
             nn.Flatten(),
-            nn.Linear(136, 1)
+            nn.Linear(968, 1)
         )
 
     def forward(self, x):
-        return self.fc(x)
+        x = self.fc(x)
+        return x
