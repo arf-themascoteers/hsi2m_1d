ca0c793c6d7b009b157d59bddbec6a5a7c6d4943
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2021-12-20 00:55:33 +1100

    Done

--- machine.py
+++ machine.py
@@ -4,12 +4,12 @@ import torch.nn as nn
 class Machine(nn.Module):
     def __init__(self):
         super(Machine, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(125,16),
-            nn.LeakyReLU(),
-            nn.Linear(16,1)
-        )
+        self.fc1 = nn.Linear(125,16)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(16, 1)
 
     def forward(self, x):
-        x = self.fc(x)
-        return x
+        x1 = self.fc1(x)
+        x2 = self.relu(x1)
+        x3 = self.fc2(x2)
+        return x3
--- sal.py
+++ sal.py
@@ -1,16 +1,55 @@
 import torch
 import matplotlib.pyplot as plt
 from data_reader import DataReader
+from machine import Machine
 
-def test(model, data, y_true):
+
+def train():
+    model = Machine()
+    criterion = torch.nn.MSELoss(reduction='mean')
+    optimiser = torch.optim.Adam(model.parameters(), lr=0.1)
+    dr = DataReader()
+    x_train, y_train, _, _ = dr.get_data()
+    x_train = x_train.reshape(x_train.shape[0], -1)
+    y_train = y_train.reshape(-1,1)
+    for t in range(300):
+        y_train_pred = model(x_train)
+        loss = criterion(y_train_pred, y_train)
+        print("Epoch ", t, "MSE: ", loss.item())
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    torch.save(model, "models/machine.h5")
+
+
+def test():
+    model = Machine()
+    criterion = torch.nn.MSELoss(reduction='mean')
+    #if not os.path.isfile("models/machine.h5"):
+    #train.train()
+    model = torch.load("models/machine.h5")
+
+    dr = DataReader()
+    _, _, x_test, y_test = dr.get_data()
+    x_test = x_test.reshape(x_test.shape[0], -1)
+    y_test = y_test.reshape(-1, 1)
+    y_test_pred = model(x_test)
+    loss = criterion(y_test_pred, y_test).item()
+    print(f"Test Loss {loss:.2f}")
+
+
+def gen_sal(data, y_true):
     model = torch.load("models/machine.h5")
     model.train()
     data = data.clone()
     data.requires_grad = True
     y_pred = model(data)
     loss = criterion(y_pred, y_true)
-    loss.backward()
+    accuracy = 1/loss
+    accuracy.backward()
     x = torch.abs(data.grad)
+
     std = torch.std(x)
     mean = torch.mean(x)
 
@@ -19,10 +58,10 @@ def test(model, data, y_true):
     x = x.squeeze().squeeze()
     plt.plot(x)
     plt.show()
-    xnp = x.numpy()
-    print(xnp)
 
 if __name__ == "__main__":
+    train()
+    test()
     model = torch.load("models/machine.h5")
     criterion = torch.nn.MSELoss(reduction='mean')
     model.eval()
@@ -31,13 +70,14 @@ if __name__ == "__main__":
     x_test = x_test.reshape(x_test.shape[0], -1)
     y_test = y_test.reshape(-1, 1)
     items = 0
+
     for i in range(len(y_test)):
         current_x = x_test[i].unsqueeze(dim=0)
         current_y = y_test[i].unsqueeze(dim=0)
         y_test_pred = model(current_x)
         loss = criterion(y_test_pred, current_y).item()
         if loss < 0.02:
-            test(model, current_x, current_y)
+            gen_sal(current_x, current_y)
             items += 1
             if items >= 5:
                 break
